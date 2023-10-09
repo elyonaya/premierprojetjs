b7e7d68a7008f261f0f02135404931e3c87c4e32
elyonaya <elyona.laurent1@gmail.com>
2023-10-09 14:05:30 +0200

    ajout js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let color = "pink";
+
+function setBgColor() {
+    document.getElementById("bg").style.backgroundColor=color;
+    document.getElementById("bgcolor").innerHTML=color;
+    document.getElementById("bgcolor").style.color=color;
+}
