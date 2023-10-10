3294e255e4626abc6b0b8aa556af97eb4a796f5a
elyonaya <elyona.laurent1@gmail.com>
2023-10-10 12:08:33 +0200

    ajout tableau dans la const color

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-let color = "pink";
+const color = ["pink", "blue", "red", "purple"];
 
 function setBgColor() {
     document.getElementById("bg").style.backgroundColor=color;
     document.getElementById("bgcolor").innerHTML=color;
     document.getElementById("bgcolor").style.color=color;
 }
+
+document.getElementById("bgbutton").addEventListener("click", setBgColor)
